82149c4e8dbcf9896ddc517be7c713d631aeffeb
Luke Kavanagh <ltkavanagh@gmail.com>
2014-02-24 10:56:32 +0000

    Build test suites from a json file

--- test/index.js
+++ test/index.js
@@ -1,6 +1,16 @@
 var test = require('tape');
 var sass = require('node-sass');
 var bower = require('../bower.json');
+var suites = require('./suites.json');
+
+var testFiles = [];
+
+Object.keys(suites).forEach(function (suite) {
+  var files = suites[suite].map(function (file) {
+    return [suite + ' ' + file, __dirname + '/' + file + '.scss'];
+  });
+  testFiles = testFiles.concat(files);
+});
 
 function fail(t, message) {
   return function() {
@@ -41,13 +51,18 @@ test('can unsilence', function (t) {
   });
 });
 
-test('usage test compiles', function (t) {
-  t.plan(1);
-  sass.render({ 
-    file: __dirname + '/test', 
-    success: function(css) {
-      t.ok(css, 'Test file compiles and contains CSS');
-    },
-    error: fail(t)
+
+testFiles.forEach(function (e) {
+  var testName = e[0];
+  var file = e[1];
+  test(testName, function (t) {
+    t.plan(1);
+    sass.render({ 
+      file: file, 
+      success: function(css) {
+        t.ok(css, testName + ' compiles and contains CSS');
+      },
+      error: fail(t)
+    });
   });
 });
